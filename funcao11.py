0d87e240d7a2b5099e30248ea029b59674e5a5a5
Veridiano1976 <37750552+Veridiano1976@users.noreply.github.com>
2018-04-25 14:10:51 -0300

    Add files via upload

---
+++ funcao11.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def funcao11():
+    celsius = int(input("Digite a temperatura em celsius\n"))
+    farenheit = celsius*1.8 + 32
+    print("A temperatura em farenheit eh: " +str(farenheit))
+
+funcao11()
---
+++ funcao12.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def funcao12():
+    farenheit = int(input("Digite a temperatura em farenheit\n"))
+    celsius = (farenheit - 32 ) / 1.8
+    print("A temperatura em celsius eh : "+str(celsius))
+
+funcao12()
