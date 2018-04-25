03f2609e86fb3458d3dcd6da9bcbccd42c9be6f2
Veridiano1976 <37750552+Veridiano1976@users.noreply.github.com>
2018-04-25 10:16:31 -0300

    Add files via upload

---
+++ funcao5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def funcao5():
+    so  = "so"
+    trabalho = " trabalho"
+    sem = " sem"
+    diversao = " diversao"
+    faz = " faz"
+    de = " de"
+    joao = " joao"
+    em = " em"
+    chato = " chato"
+    print(so+trabalho+sem+diversao+faz+de+joao+em+chato)
+    
+funcao5()
